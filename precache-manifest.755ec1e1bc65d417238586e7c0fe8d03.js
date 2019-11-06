dbae13380d5b5f3723db8cd3c2850022072084d4
MarielaTorresB <martz_maru@hotmail.com>
2019-11-06 12:30:25 -0600

    deploy

---
+++ precache-manifest.755ec1e1bc65d417238586e7c0fe8d03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7d0bf19c185f42fb4c",
+    "url": "/RS-con--Vue/css/chunk-vendors.de348ef2.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/RS-con--Vue/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/RS-con--Vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c33ca4d8aff1ab2d39369b4f305f2e7d",
+    "url": "/RS-con--Vue/index.html"
+  },
+  {
+    "revision": "b20360569855a5106837",
+    "url": "/RS-con--Vue/js/about.fc628091.js"
+  },
+  {
+    "revision": "262ca13893cf4e094fd5",
+    "url": "/RS-con--Vue/js/app.3b703137.js"
+  },
+  {
+    "revision": "7d7d0bf19c185f42fb4c",
+    "url": "/RS-con--Vue/js/chunk-vendors.5ad1e89b.js"
+  },
+  {
+    "revision": "a9cf37642235ac994f549f336a54e8b9",
+    "url": "/RS-con--Vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/RS-con--Vue/robots.txt"
+  }
+]);
